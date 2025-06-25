3f344e014643e3d5c7a03997edb5515cbb71058a
harshitha puvvada <114869426+Harshithapuvvada@users.noreply.github.com>
2025-06-25 11:34:57 +0530

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>hello world</h1>
+    <h2>my first react app on aws EC2</h2>
+    <h3>deployed by harshitha puvvada</h3>
+  </>
+)
+
+}
+
+export default App
